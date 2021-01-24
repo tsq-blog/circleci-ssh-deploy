243d5cec4ff94969bfc2c3a92da6498602c783d5
TANG SHI QIANG <tangshiqiangcn@gmail.com>
2021-01-25 00:43:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('5');
+    res.end('10');
 });
 
 const port = 8088;
