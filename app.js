f15f0fc51e1a80cc64fb6fa8627211611d18fb3e
tsq <tangshiqiangcn@gmail.com>
2021-01-24 21:41:08 +0900

    update config.yml

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('2');
+    res.end('3');
 });
 
 const port = 8088;
